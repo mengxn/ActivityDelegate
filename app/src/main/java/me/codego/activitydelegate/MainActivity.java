d00f957dc2aef8b02b2f4106fe93e1ef3b9e5509
mengxn <mengxn@outlook.com>
2017-11-30 17:17:55 +0800

    modify demo

--- app/src/main/java/me/codego/activitydelegate/MainActivity.java
+++ app/src/main/java/me/codego/activitydelegate/MainActivity.java
@@ -1,13 +1,20 @@
 package me.codego.activitydelegate;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.Toast;
 
+import me.codego.utils.ActivityResponse;
 import me.codego.utils.PIntent;
 
 
 public class MainActivity extends AppCompatActivity {
+
+    private ActivityResponse mActivityResponse;
+
+    private static final int REQUEST_OPEN_SECOND = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -38,5 +45,31 @@ public class MainActivity extends AppCompatActivity {
         PIntent.from(this)
                 .transition(R.anim.slide_in_right, R.anim.slide_out_left)
                 .to("me.codego.activitydelegate.action.SECOND");
+    }
+
+    public void openSecondWithShareView(View view) {
+        PIntent.from(this)
+                .with("key", "open with share view")
+                .share(view, "share")
+                .to(SecondActivity.class);
+    }
+
+    public void openSecondWithCallback(View view) {
+        mActivityResponse = PIntent.from(this)
+                .to(SecondActivity.class, REQUEST_OPEN_SECOND)
+                .result(new ActivityResponse.Callback() {
+                    @Override
+                    public void onResult(Intent data) {
+                        Toast.makeText(MainActivity.this, data.getStringExtra("text"), Toast.LENGTH_SHORT).show();
+                    }
+                });
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (mActivityResponse != null) {
+            mActivityResponse.apply(requestCode, resultCode, data);
+        }
     }
 }
--- app/src/main/java/me/codego/activitydelegate/SecondActivity.java
+++ app/src/main/java/me/codego/activitydelegate/SecondActivity.java
@@ -1,5 +1,6 @@
 package me.codego.activitydelegate;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.KeyEvent;
@@ -15,8 +16,8 @@ public class SecondActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        PIntent.applyScene(this);
         setContentView(R.layout.activity_second);
-
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
         Bundle extras = getIntent().getExtras();
         if (extras != null && extras.size() > 0) {
@@ -25,6 +26,7 @@ public class SecondActivity extends AppCompatActivity {
     }
 
     public void closeWithAnimation(View view) {
+        setResult(RESULT_OK, new Intent().putExtra("text", "something with second"));
         PIntent.from(this).finish();
     }
 
@@ -33,6 +35,8 @@ public class SecondActivity extends AppCompatActivity {
         switch (item.getItemId()) {
             case android.R.id.home:
                 PIntent.from(this).finish();
+                break;
+            default:
                 break;
         }
         return super.onOptionsItemSelected(item);
@@ -44,6 +48,8 @@ public class SecondActivity extends AppCompatActivity {
             case KeyEvent.KEYCODE_BACK:
                 PIntent.from(this).finish();
                 return true;
+            default:
+                break;
         }
         return super.onKeyDown(keyCode, event);
     }
